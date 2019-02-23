0e36deb6b9d02c3878734c57335d41c4f0114273
galubezzz <galubezzz@gmail.com>
2019-02-22 16:14:46 -0800

    display the list of the countries

--- src/containers/Countries/Countries.js
+++ src/containers/Countries/Countries.js
@@ -1,7 +1,28 @@
 import React, {Component, Fragment} from 'react';
+import axios from 'axios';
 
 class Countries extends Component {
+    state = {
+        countries: [],
+    };
 
+    componentDidMount() {
+        axios.get().then(response => {
+            return response;
+        }).then(response => {
+            this.setState({countries: response.data});
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+
+    render() {
+        let countries = this.state.countries;
+        return (
+            countries.map(country => (<div>{country.name}</div>))
+        );
+    }
 }
 
 export default Countries;
