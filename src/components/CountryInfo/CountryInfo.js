f5f5fa9f9770e92b666f440958ce97fd60cbe496
galubezzz <galubezzz@gmail.com>
2019-02-24 20:32:38 -0800

    add the displa for border countries

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -5,7 +5,8 @@ import './CountryInfo.css';
 class CountryInfo extends Component {
 
     state = {
-        loadedCountry: null
+        loadedCountry: null,
+        borderCountries: [],
     };
 
     componentDidUpdate() {
@@ -19,6 +20,11 @@ class CountryInfo extends Component {
                 axios.get(baseURL + this.props.code)
                     .then(response => {
                         this.setState({loadedCountry: response.data});
+                        const requests = this.state.loadedCountry.borders.map(borderCountry => axios.get(baseURL + borderCountry).then(response => {return response.data}
+                        ))
+                        Promise.all(requests).then(countries => {
+                            this.setState({borderCountries: countries})
+                        })
                     })
                     .catch(error => {
                         console.log(error);
@@ -31,11 +37,17 @@ class CountryInfo extends Component {
         return (
             this.state.loadedCountry ?
                 <div className="country-info-main-container">
-                  <div><img alt="country flag" className="img" src={this.state.loadedCountry.flag}/></div>
-                  <p>{this.state.loadedCountry.name}</p>
-                  <p>Population: {this.state.loadedCountry.population}</p>
-                  <p>Capital: {this.state.loadedCountry.capital}</p>
-                  <p>Region: {this.state.loadedCountry.region}</p>
+                    <div><img alt="country flag" className="img" src={this.state.loadedCountry.flag}/></div>
+                    <p>{this.state.loadedCountry.name}</p>
+                    <p>Population: {this.state.loadedCountry.population}</p>
+                    <p>Capital: {this.state.loadedCountry.capital}</p>
+                    <p>Region: {this.state.loadedCountry.region}</p>
+                    {!!this.state.borderCountries.length &&
+                    <div> Borders with:
+                        <ul>
+                            {this.state.borderCountries.map(country => <li>{country.name}</li>)}
+                        </ul>
+                    </div>}
                 </div> : null
         )
     }
